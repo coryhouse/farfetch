102a2dc1d98be049d0900b49b24e51a82ea5c463
Cory House <Cory.House@coxautoinc.com>
2018-01-14 21:39:45 -0600

    Enhance test coverage

--- src/utils/numberFormat.js
+++ src/utils/numberFormat.js
@@ -1,11 +1,11 @@
-import {roundNumber} from './math';
+import { roundNumber } from "./math";
 
 export function getCurrencyFormattedNumber(value) {
-  if (value === null) {
-    return '';
+  if (!value) {
+    return "";
   }
 
-  return '$' + getFormattedNumber(value); // eslint-disable-line prefer-template
+  return "$" + getFormattedNumber(value); // eslint-disable-line prefer-template
 }
 
 export function getFormattedNumber(value) {
@@ -13,24 +13,19 @@ export function getFormattedNumber(value) {
     return 0;
   }
 
-  if (!value) {
-    return '';
-  }
-
   if (!isInt(scrubFormatting(value))) {
-    return ''; // if it's not a number after scrubbing formatting, just return empty.
+    return null; // if it's not a number after scrubbing formatting, just return null.
   }
 
   let roundedValue = roundNumber(value, 2); // round if more than 2 decimal points
-  roundedValue = roundedValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','); // add commas for 1,000's. RegEx from http://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
-  const roundedValueContainsDecimalPlace = (roundedValue.indexOf('.') !== -1);
+  roundedValue = roundedValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); // add commas for 1,000's. RegEx from http://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
+  console.log("rounded " + value + " to " + roundedValue);
+  const roundedValueContainsDecimalPlace = roundedValue.indexOf(".") !== -1;
 
   if (roundedValueContainsDecimalPlace) {
-    const numbersToTheRightOfDecimal = roundedValue.split('.')[1];
+    const numbersToTheRightOfDecimal = roundedValue.split(".")[1];
 
     switch (numbersToTheRightOfDecimal.length) {
-      case 0:
-        return roundedValue.replace('.', ''); // no decimal necessary since no numbers after decimal
       case 1:
         return `${roundedValue}0`;
       default:
@@ -41,7 +36,7 @@ export function getFormattedNumber(value) {
 }
 
 export function isInt(n) {
-  if (n === '' || n === null) {
+  if (n === "" || n === null) {
     return false;
   }
 
@@ -49,5 +44,9 @@ export function isInt(n) {
 }
 
 export function scrubFormatting(value) {
-  return value.toString().replace('$', '').replace(',', '').replace('.', '');
+  return value
+    .toString()
+    .replace("$", "")
+    .replace(",", "")
+    .replace(".", "");
 }
--- src/utils/numberFormat.spec.js
+++ src/utils/numberFormat.spec.js
@@ -1,39 +1,56 @@
-import {getCurrencyFormattedNumber, isInt, scrubFormatting, getFormattedNumber} from './numberFormat';
+import {
+  getCurrencyFormattedNumber,
+  isInt,
+  scrubFormatting,
+  getFormattedNumber
+} from "./numberFormat";
 
-describe('Number Formatter', () => {
-  describe('getCurrencyFormattedNumber', () => {
-    it('returns $5.50 when passed 5.5', () => {
-      expect(getCurrencyFormattedNumber(5.5)).toEqual('$5.50');
+describe("Number Formatter", () => {
+  describe("getCurrencyFormattedNumber", () => {
+    it("returns $5.50 when passed 5.5", () => {
+      expect(getCurrencyFormattedNumber(5.5)).toEqual("$5.50");
+    });
+
+    it("returns $1.00 when passed 1", () => {
+      expect(getCurrencyFormattedNumber(5.5)).toEqual("$1.00");
+    });
+
+    it.only("returns $0.50 when passed 0.5", () => {
+      expect(getCurrencyFormattedNumber(0.5)).toEqual("$0.50");
+    });
+
+    it("returns empty string if passed an empty string", () => {
+      expect(getCurrencyFormattedNumber("")).toEqual("");
     });
   });
 
-  describe('isInt', () => {
-    it('returns true when passed 0', () => {
+  describe("isInt", () => {
+    it("returns true when passed 0", () => {
       expect(isInt(0)).toEqual(true);
     });
 
-    it('returns false when passed an empty string', () => {
-      expect(isInt('')).toEqual(false);
+    it("returns false when passed an empty string", () => {
+      expect(isInt("")).toEqual(false);
     });
 
-    it('returns true when passed int as a string', () => {
-      expect(isInt('5')).toEqual(true);
-    });
-  });
-
-  describe('scrubFormatting', () => {
-    it('strips commas and decimals', () => {
-      expect(scrubFormatting('1,234.56')).toEqual('123456');
+    it("returns true when passed int as a string", () => {
+      expect(isInt("5")).toEqual(true);
     });
   });
 
-  describe('getFormattedNumber', () => {
-    it('returns 0 if passed 0', () => {
+  describe("scrubFormatting", () => {
+    it("strips commas and decimals", () => {
+      expect(scrubFormatting("1,234.56")).toEqual("123456");
+    });
+  });
+
+  describe("getFormattedNumber", () => {
+    it("returns 0 if passed 0", () => {
       expect(getFormattedNumber(0)).toEqual(0);
     });
 
-    it('returns empty string if passed empty string', () => {
-      expect(getFormattedNumber('')).toEqual('');
+    it("returns empty null if passed empty string", () => {
+      expect(getFormattedNumber("")).toEqual(null);
     });
   });
 });
