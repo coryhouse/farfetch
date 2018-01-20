bff0f9471cbefae6cf1e6c2f7c2253d2b6cf5b44
Cory House <Cory.House@coxautoinc.com>
2018-01-20 09:56:34 -0600

    Enhance with support for internal folder and debounce

--- scripts/generateComponentData.js
+++ scripts/generateComponentData.js
@@ -1,75 +1,13 @@
-var fs = require('fs');
-var path = require('path');
-var chalk = require('chalk');
-var parse = require('react-docgen').parse;
-var chokidar = require('chokidar');
-
-var paths = {
-  examples: path.join(__dirname, '../src', 'docs', 'examples'),
-  components: path.join(__dirname, '../src', 'components'),
-  output: path.join(__dirname, '../config', 'componentData.js')
-};
-
-const enableWatchMode = process.argv.slice(2) == '--watch';
-if (enableWatchMode) {
-  // Regenerate component metadata when components or examples change.
-  chokidar.watch([paths.examples, paths.components]).on('change', function(event, path) {
-    generate(paths);
-  });
-} else {
-  // Generate component metadata
-  generate(paths);
-}
-
-function generate(paths) {
-  var errors = [];
-  var componentData = getDirectories(paths.components).map(function(componentName) {
-    try {
-      return getComponentData(paths, componentName)
-    } catch(error) {
-      errors.push('An error occurred while attempting to generate metadata for ' + componentName + '. ' + error);
-    }
-  });
-  writeFile(paths.output, "module.exports = /* eslint-disable */ " + JSON.stringify(errors.length ? errors : componentData));
-}
-
-function getComponentData(paths, componentName) {
-  var content = readFile(path.join(paths.components, componentName, componentName + '.js'));
-  var info = parse(content);
-  return {
-    name: componentName,
-    description: info.description,
-    props: info.props,
-    code: content,
-    examples: getExampleData(paths.examples, componentName)
-  }
-}
-
-function getExampleData(examplesPath, componentName) {
-  var examples = getExampleFiles(examplesPath, componentName);
-  return examples.map(function(file) {
-    var filePath = path.join(examplesPath, componentName, file)
-    var content = readFile(filePath)
-    var info = parse(content);
-    return {
-      // By convention, component name should match the filename.
-      // So remove the .js extension to get the component name.
-      name: file.slice(0, -3),
-      description: info.description,
-      code: content
-    };
-  });
-}
-
-function getExampleFiles(examplesPath, componentName) {
-  var exampleFiles = [];
-  try {
-    exampleFiles = getFiles(path.join(examplesPath, componentName));
-  } catch(error) {
-    console.log(chalk.red(`No examples found for ${componentName}.`));
-  }
-  return exampleFiles;
-}
+/* This script reads the components in src/components
+ * and the examples in docs/examples and generates metadata
+ * that is used to display our docs.
+ */
+const fs = require("fs");
+const path = require("path");
+const chalk = require("chalk");
+const parse = require("react-docgen").parse;
+const chokidar = require("chokidar");
+const debounce = require("debounce");
 
 function getDirectories(filepath) {
   return fs.readdirSync(filepath).filter(function(file) {
@@ -84,11 +22,102 @@ function getFiles(filepath) {
 }
 
 function writeFile(filepath, content) {
-  fs.writeFile(filepath, content, function (err) {
-    err ? console.log(chalk.red(err)) : console.log(chalk.green("Component data saved."));
+  fs.writeFile(filepath, content, function(err) {
+    err
+      ? console.log(chalk.red(err))
+      : console.log(chalk.green("Component data saved."));
   });
 }
 
 function readFile(filePath) {
-  return fs.readFileSync(filePath, 'utf-8');
+  return fs.readFileSync(filePath, "utf-8");
+}
+
+function getExampleFiles(examplesPath, componentName) {
+  let exampleFiles = [];
+  try {
+    exampleFiles = getFiles(path.join(examplesPath, componentName));
+  } catch (error) {
+    console.log(chalk.red(`No examples found for ${componentName}.`));
+  }
+  return exampleFiles;
+}
+
+function getExampleData(examplesPath, componentName) {
+  const examples = getExampleFiles(examplesPath, componentName);
+  return examples.map(function(file) {
+    const filePath = path.join(examplesPath, componentName, file);
+    const content = readFile(filePath);
+    const info = parse(content);
+    return {
+      // By convention, component name should match the filename.
+      // So remove the .js extension to get the component name.
+      name: file.slice(0, -3),
+      description: info.description,
+      code: content
+    };
+  });
+}
+
+function getComponentData(paths, componentName) {
+  // check for Icons.
+  const fileName = componentName === "Icons" ? "IconNote" : componentName;
+  const content = readFile(
+    path.join(paths.components, componentName, fileName + ".js")
+  );
+  const info = parse(content);
+  return {
+    name: componentName,
+    description: info.description,
+    props: info.props,
+    code: content,
+    examples: getExampleData(paths.examples, componentName)
+  };
+}
+
+function generate(paths) {
+  const errors = [];
+  const componentData = getDirectories(paths.components).map(function(
+    componentFolder
+  ) {
+    if (componentFolder === "internal") {
+      return "";
+    } else {
+      try {
+        return getComponentData(paths, componentFolder);
+      } catch (error) {
+        errors.push(
+          "An error occurred while attempting to generate metadata for " +
+            componentFolder +
+            ". " +
+            error
+        );
+      }
+    }
+    return "";
+  });
+  writeFile(
+    paths.output,
+    "module.exports = " + JSON.stringify(errors.length ? errors : componentData)
+  );
+}
+
+const paths = {
+  examples: path.join(__dirname, "../src", "docs", "examples"),
+  components: path.join(__dirname, "../src", "components"),
+  output: path.join(__dirname, "../config", "componentData.js")
+};
+
+const enableWatchMode = process.argv.slice(2).toString() === "--watch";
+
+if (enableWatchMode) {
+  // Regenerate component metadata when components or examples change.'
+  chokidar
+    .watch([paths.examples, paths.components])
+    .on("change", function(event, path) {
+      debounce(generate(paths), 1000); // Run no more than once a second.
+    });
+} else {
+  // Generate component metadata
+  generate(paths);
 }
