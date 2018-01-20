bff42ec1c274df8edbb0a12098157f228bebf431
Cory House <Cory.House@coxautoinc.com>
2018-01-20 09:40:03 -0600

    Initial commit

---
+++ src/docs/CodeExample.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "../prism";
+import { PrismCode } from "react-prism";
+import "prismjs/themes/prism-okaidia.css";
+
+class CodeExample extends React.Component {
+  render() {
+    return (
+      <pre
+        ref={ref => {
+          this.element = ref;
+        }}
+      >
+        <PrismCode className="language-jsx">{this.props.children}</PrismCode>
+      </pre>
+    );
+  }
+}
+
+CodeExample.propTypes = {
+  children: PropTypes.string.isRequired
+};
+
+export default CodeExample;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Docs from "./docs/Docs";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
+
+ReactDOM.render(<Docs />, document.getElementById("root"));
+registerServiceWorker();
